02c6d7b3185ba6b4b8782ba6e9d2eb59897bc943
Qadeer Hussain <qadeeerhussain494@gmail.com>
2021-04-03 11:28:55 +0500

    Just Practice

---
+++ app.js
@@ -0,0 +1,122 @@
+// var x;
+// for(x=1;x<=20;x=x+2){
+//     console.log(x)
+// }
+
+// var i;
+// for(i=1;i<=10;i++){
+//     console.log("2"+"X"+i+"="+2*i);
+//     document.write("2"+"X"+i+"="+2*i+"<br>")
+// }
+
+// var n=["Qadeer","Muzahir","Hadi","Muhammad","Ali"];
+// for(var i =0;i<n.length;i++){
+//     console.log(n[i])
+//     document.write(n[i]+"<br>")
+// }
+
+
+// var n=["Qadeer","Muzahir","Hadi","Muhammad","Ali"];
+// for(var i =0;i<n.length;i++){
+//     if(n[i]=="Ali"){
+//         alert(n[i]+" "+"is a good Boy")
+//     }
+// }
+
+
+// for(var i=1;i>0;i++){
+//     if(i==10){
+//         break
+//     }
+//     document.write("hello"+"<br>")
+//     console.log("hello")
+// }
+
+
+// for(var i=0;i<5;i++){
+//     for(var j=0;j<3;j++){
+//         document.write(i+" "+j+"Pakistan"+"<br>")
+//         console.log("Pakistan")
+
+//     }
+// }
+
+// var f=["A","B","C","D"]
+// var l=["E","F"]
+// for(var i=0;i<f.length;i++){
+//     for(var x=0;x<l.length;x++){
+//        console.log(f[i]+" "+l[x])
+//        document.write(f[i]+" "+l[x]+"<br>") 
+//     }
+// }
+
+
+// for(var i=0; i<10;i++){
+//     for(var j=0;j>i;j++){
+//         document.write("*")
+
+//     }
+//     document.write("<br>")
+// }
+
+
+
+// var w=prompt("enter word")
+// for(var i=w.length -1;i>=0;i--){
+//     console.log(w[i])
+// }
+
+
+// var w=prompt("enter word");
+// var check=" ";
+// for(var i=w.length - 1;i>=0;i--){
+//     check +=w[i]
+// }
+// if(w===check){
+//     console.log(w+" "+"Is Palindrome Word")
+// }
+// else{
+//     console.log("Not Palindrome word")    
+// }
+
+// var n="qadeer HUSSAIN"
+// //n=n.toLowerCase();
+// n=n.toUpperCase();
+// console.log(n);
+
+// var n="Qadeer Hussain";
+// n=n.indexOf("H");
+// console.log(n)
+
+
+
+
+
+
+
+
+
+// var x=prompt("Enter 1st Value");
+// var a=prompt("Enter Operator");
+// var y=prompt("Enter 2nd Value");
+// var z;
+// if(a=='+'){
+//     z=x+y;
+//     alert(z)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
